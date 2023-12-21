d1f607d893c46876f6c628c7031f5cbd3c00f3cb
Samuel Sciolla <ssciolla@umich.edu>
2023-12-21 10:59:58 -0500

    Move BagAdapter to its own module

--- lib/bag_courier.rb
+++ lib/bag_courier.rb
@@ -1,8 +1,8 @@
 require "logger"
 
-require "bagit"
 require "minitar"
 
+require_relative "bag_adapter"
 require_relative "remote"
 require_relative "status_event"
 
@@ -10,52 +10,6 @@ LOGGER = Logger.new($stdout)
 
 module BagCourier
   Work = Struct.new("Work", :id, :creator, :description, :title)
-
-  class BagAdapter
-    def initialize(target_dir)
-      @bag = BagIt::Bag.new(target_dir)
-      @additional_tag_files = []
-    end
-
-    def bag_dir
-      @bag.bag_dir
-    end
-
-    def data_dir
-      @bag.data_dir
-    end
-
-    def add_bag_info(bag_info_data)
-      @bag.write_bag_info(bag_info_data)
-    end
-
-    def add_tag_file!(tag_file_text:, file_name:)
-      file = File.join(@bag.bag_dir, file_name)
-      File.write(file, tag_file_text, mode: "w")
-      @additional_tag_files << file
-    end
-
-    def add_manifests
-      LOGGER.debug([
-        "bag.tag_files=#{@bag.tag_files}",
-        "additional_tag_files=#{@additional_tag_files}"
-      ])
-      @bag.manifest!(algo: "md5") # Create manifests
-
-      # Rewrite the tag manifest files to include extra tag files
-      tag_files = @bag.tag_files
-      new_tag_files = tag_files & @additional_tag_files
-      LOGGER.debug("new_tag_files=#{new_tag_files}")
-      @bag.tagmanifest!(new_tag_files) unless (new_tag_files - tag_files).empty?
-
-      # HELIO-4380 demo.aptrust.org doesn't like this file for some reason, gives an ingest error:
-      # "Bag contains illegal tag manifest 'sha1'""
-      # APTrust only wants SHA256, or MD5, not SHA1.
-      # 'tagmanifest-sha1.txt' is a bagit gem default, so we need to remove it manually.
-      sha1tag = File.join(@bag.bag_dir, "tagmanifest-sha1.txt")
-      File.delete(sha1tag) if File.exist?(sha1tag)
-    end
-  end
 
   class BagId
     attr_reader :repository, :object_id, :context, :part_id
@@ -172,7 +126,7 @@ module BagCourier
         track!(status: "depositing")
         track!(status: "bagging")
         bag_path = File.join(@working_dir, @bag_id.to_s)
-        bag = BagAdapter.new(bag_path)
+        bag = BagAdapter::BagAdapter.new(bag_path)
 
         track!(status: "copying")
         @data_transfer.transfer(bag.data_dir)
