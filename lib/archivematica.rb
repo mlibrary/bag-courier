5517d7292e252a36fcda634b5ca5f55b935ee4d5
Samuel Sciolla <ssciolla@umich.edu>
2024-01-22 10:46:30 -0500

    Use API filtering mechanism

--- lib/archivematica.rb
+++ lib/archivematica.rb
@@ -33,13 +33,13 @@ module Archivematica
     end
     private :get_next_url
 
-    def get_objects_from_pages(url)
+    def get_objects_from_pages(url, params = nil)
       no_more_pages = false
       current_url = url
       results = []
       LOGGER.debug("Starting URL: #{current_url}")
       until no_more_pages
-        resp = @conn.get(current_url)
+        resp = @conn.get(current_url, params)
         data = JSON.parse(resp.body)
         results += data["objects"]
         meta = data["meta"]
@@ -55,14 +55,13 @@ module Archivematica
     end
     private :get_objects_from_pages
 
-    def get_packages(uuid)
-      objects = get_objects_from_pages(@@packages_endpoint)
+    def get_packages(location_uuid:)
+      objects = get_objects_from_pages(@@packages_endpoint, {
+        "current_location" => location_uuid
+      })
       LOGGER.debug("Total number of objects found: #{objects.length}")
-      filtered_objects = objects.select do |o|
-        location_url = "#{@api_prefix}#{@@location_endpoint}#{uuid}/"
-        o["current_location"] == location_url && o["status"] == "UPLOADED"
-      end
-      packages = filtered_objects.map do |o|
+      uploaded_objects = objects.select { |o| o["status"] == "UPLOADED" }
+      packages = uploaded_objects.map do |o|
         Package.new(
           uuid: o["uuid"],
           path: o["current_full_path"],
@@ -70,7 +69,10 @@ module Archivematica
           stored_date: o["stored_date"]
         )
       end
-      LOGGER.debug("Number of packages found in target location with UPLOADED status: #{packages.length}")
+      LOGGER.info(
+        "Number of packages found in target location with UPLOADED status: " +
+        packages.length.to_s
+      )
       packages
     end
   end
