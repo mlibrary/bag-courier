e72f8981be4dad026ea0e72e1f33e518a3275f00
Samuel Sciolla <ssciolla@umich.edu>
2024-02-28 16:29:28 -0500

    Introduce Sequel::Model; add more explicit assertion

---
+++ db/schema.rb
@@ -0,0 +1,16 @@
+require "sequel"
+
+module Schema
+  class Bag < Sequel::Model(:bag)
+    one_to_many :status_events
+  end
+
+  class Status < Sequel::Model(:status)
+    one_to_many :status_events
+  end
+
+  class StatusEvent < Sequel::Model(:status_event)
+    many_to_one :status
+    many_to_one :bag
+  end
+end
