0786ee70f6fbe0277282a39a4bcd5bd6305cb619
Samuel Sciolla <ssciolla@umich.edu>
2024-02-28 10:02:49 -0500

    Remove DirDataTransfer and import

--- lib/data_transfer.rb
+++ lib/data_transfer.rb
@@ -1,22 +1,7 @@
-require_relative "remote_client"
-
 module DataTransfer
   class DataTransferBase
     def transfer(target_dir)
       raise NotImplementedError
-    end
-  end
-
-  class DirDataTransfer < DataTransferBase
-    attr_reader :source_dir
-
-    def initialize(source_dir)
-      @source_dir = source_dir
-    end
-
-    def transfer(target_dir)
-      RemoteClient::FileSystemRemoteClient.new(@source_dir)
-        .retrieve_from_path(local_path: target_dir)
     end
   end
 
