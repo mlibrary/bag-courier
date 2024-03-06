7c8f4e6f6bb317c1e3bec6de7887d9076703f41f
Samuel Sciolla <ssciolla@umich.edu>
2024-03-06 17:00:49 -0500

    Introduce APIBackend; copy/modify relevant tests from ArchivematicaAPI

---
+++ lib/api_backend.rb
@@ -0,0 +1,35 @@
+require "faraday"
+require "faraday/retry"
+
+module APIBackend
+  class APIError < StandardError
+  end
+
+  class APIBackendBase
+    def get
+      raise NotImplementedError
+    end
+  end
+
+  class FaradayAPIBackend < APIBackendBase
+    def initialize(base_url:, headers:, &b)
+      @base_url = base_url
+      @conn = Faraday.new(url: base_url, headers: headers) do |builder|
+        builder.request :retry
+        builder.response :raise_error
+        b&.call(builder)
+      end
+    end
+
+    def get(url, params = nil)
+      resp = @conn.get(url, params)
+      JSON.parse(resp.body)
+    rescue Faraday::Error => error
+      message = "Error occurred while interacting with REST API at #{@base_url}. " \
+        "Error type: #{error.class}; " \
+        "status code: #{error.response_status || "none"}; " \
+        "body: #{error.response_body || "none"}"
+      raise APIError, message
+    end
+  end
+end
---
+++ test/test_api_backend.rb
@@ -0,0 +1,83 @@
+require "faraday"
+require "faraday/retry"
+require "minitest/autorun"
+require "minitest/pride"
+
+require_relative "../lib/api_backend"
+
+class FaradayAPIBackendTest < Minitest::Test
+  include APIBackend
+
+  def setup
+    @base_url = "http://some-service.org/api/v1/"
+    @headers = {"Authorization" => "ApiKey youruser:some-secret-key"}
+    @stubs = Faraday::Adapter::Test::Stubs.new
+    @stubbed_api_backend = FaradayAPIBackend.new(
+      base_url: @base_url, headers: @headers
+    ) do |builder|
+      builder.adapter :test, @stubs
+    end
+  end
+
+  def test_new_works_without_block
+    backend = FaradayAPIBackend.new(base_url: @base_url, headers: @headers)
+    assert backend.is_a?(FaradayAPIBackend)
+    assert backend.respond_to?(:get)
+  end
+
+  def test_get_throws_unauthorized_error_with_response_info
+    @stubs.get(@base_url + "file/") do |env|
+      [401, {"Content-Type": "text/plain"}, "Unauthorized"]
+    end
+    error = assert_raises APIError do
+      @stubbed_api_backend.get("file/")
+    end
+    expected = "Error occurred while interacting with REST API at #{@base_url}. " \
+      "Error type: Faraday::UnauthorizedError; " \
+      "status code: 401; " \
+      "body: Unauthorized"
+    assert_equal expected, error.message
+  end
+
+  def test_get_retries_on_timeout_to_failure
+    calls = 0
+    @stubs.get(@base_url + "file/") do |env|
+      calls += 1
+      env[:body] = nil
+      raise Faraday::TimeoutError
+    end
+
+    # Final error is caught and transformed.
+    error = assert_raises APIError do
+      @stubbed_api_backend.get("file/")
+    end
+    expected = "Error occurred while interacting with REST API at http://some-service.org/api/v1/. " \
+      "Error type: Faraday::TimeoutError; " \
+      "status code: none; " \
+      "body: none"
+    assert_equal expected, error.message
+
+    assert_equal 3, calls
+  end
+
+  def test_get_retries_on_timeout_failing_once_then_succeeding
+    calls = 0
+    @stubs.get(@base_url + "file/") do |env|
+      env[:body] = nil
+      calls += 1
+      if calls < 2
+        raise Faraday::TimeoutError
+      else
+        [200, {"Content-Type": "application/json"}, "{}"]
+      end
+    end
+
+    data = @stubbed_api_backend.get("file/")
+    assert_equal 2, calls
+    assert_equal ({}), data
+  end
+
+  def teardown
+    Faraday.default_connection = nil
+  end
+end
