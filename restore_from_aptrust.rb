e45f4cc6c6a0b6661789be43903fee856dc533e7
Samuel Sciolla <ssciolla@umich.edu>
2024-04-22 16:37:23 -0400

    Adopt services.rb; fix bugs

--- restore_from_aptrust.rb
+++ restore_from_aptrust.rb
@@ -1,36 +1,33 @@
-require "semantic_logger"
+require "optparse"
 
-require_relative "lib/config"
 require_relative "lib/remote_client"
-
-SemanticLogger.add_appender(io: $stderr, formatter: :color)
-config = Config::ConfigService.from_file(File.join(".", "config", "config.yml"))
-SemanticLogger.default_level = config.settings.log_level
+require_relative "services"
 
 class RestoreError < StandardError
 end
 
 class RestoreJob
-  include SemanticLogger::Loggable
+  include DarkBlueLogger
 
   def initialize(config:, base_path: "umich.edu")
     aws_config = config.aptrust.remote.settings
     if config.aptrust.remote.type != :aptrust
       raise RestoreError, "The APTrust remote type must be \"aptrust\" to use the restore script."
     end
-    AwsS3RemoteClient.update_config(
+    RemoteClient::AwsS3RemoteClient.update_config(
       access_key_id: aws_config.access_key_id,
       secret_access_key: aws_config.secret_access_key
     )
     @base_path = base_path
     @restore_dir = config.settings.restore_dir
-    @aptrust_client = AwsS3RemoteClient.from_config(
+    @aptrust_client = RemoteClient::AwsS3RemoteClient.from_config(
       region: aws_config.region,
       bucket_name: aws_config.restore_bucket
     )
+    staging_remote_config = config.dark_blue.staging.remote
     @staging_client = RemoteClient::RemoteClientFactory.from_config(
-      type: config.dark_blue.staging.type,
-      settings: config.dark_blue.staging.settings
+      type: staging_remote_config.type,
+      settings: staging_remote_config.settings
     )
   end
 
@@ -43,7 +40,7 @@ class RestoreJob
   end
 
   def restore_all
-    logger.info("Retrieving all files in #{aptrust_client.remote_text}from path: #{@base_path}")
+    logger.info("Retrieving all files in #{@aptrust_client.remote_text}from path: #{@base_path}")
     @aptrust_client.retrieve_from_path(remote_path: @base_path, local_path: @restore_dir)
   end
 
@@ -57,7 +54,7 @@ class RestoreJob
   end
 end
 
-RestoreOptions = Struct.new(:bags)
+RestoreOptions = Struct.new(:bags, :send_to_staging)
 
 class RestoreParser
   def self.parse(options)
@@ -72,8 +69,13 @@ class RestoreParser
       ) do |b|
         args.bags = b
       end
-      parser.on("-s", "--send", TrueClass, "Flag determing whether to send bags to staging") do
-        args.send = s
+      parser.on(
+        "-s",
+        "--send-to-staging",
+        TrueClass,
+        "Flag determing whether to send bags to staging"
+      ) do |s|
+        args.send_to_staging = s
       end
       parser.on("-h", "--help", "Prints this help") do
         puts parser
@@ -85,7 +87,7 @@ class RestoreParser
   end
 end
 
-restore_job = RestoreJob.new(config)
+restore_job = RestoreJob.new(config: S.config)
 
 options = RestoreParser.parse ARGV
 if options.bags.length > 0
@@ -93,4 +95,4 @@ if options.bags.length > 0
 else
   restore_job.restore_all
 end
-restore_job.send_to_staging if options.send
+restore_job.send_to_staging if options.send_to_staging
