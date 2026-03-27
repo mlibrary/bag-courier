ec2041779bd4fe2e3245697cdb6c4965f6cc4b35
Samuel Sciolla <ssciolla@umich.edu>
2026-03-27 16:26:26 -0400

    Change @object_id -> @digital_object_id to address warning about redefining Ruby method

--- test/test_bag_courier.rb
+++ test/test_bag_courier.rb
@@ -17,21 +17,21 @@ class BagIdTest < Minitest::Test
   def test_to_s
     expected = "somerepo.uniqueid"
     assert_equal expected, BagCourier::BagId.new(
-      repository: "somerepo", object_id: "uniqueid"
+      repository: "somerepo", digital_object_id: "uniqueid"
     ).to_s
   end
 
   def test_to_s_with_context
     expected = "somerepo.somecontext-uniqueid"
     assert_equal expected, BagCourier::BagId.new(
-      repository: "somerepo", object_id: "uniqueid", context: "somecontext"
+      repository: "somerepo", digital_object_id: "uniqueid", context: "somecontext"
     ).to_s
   end
 
   def test_to_s_with_part
     expected = "somerepo.uniqueid-4"
     assert_equal expected, BagCourier::BagId.new(
-      repository: "somerepo", object_id: "uniqueid", part_id: "4"
+      repository: "somerepo", digital_object_id: "uniqueid", part_id: "4"
     ).to_s
   end
 
@@ -39,7 +39,7 @@ class BagIdTest < Minitest::Test
     expected = "somerepo.somecontext-uniqueid-4"
     assert_equal expected, BagCourier::BagId.new(
       repository: "somerepo",
-      object_id: "uniqueid",
+      digital_object_id: "uniqueid",
       context: "somecontext",
       part_id: "4"
     ).to_s
@@ -87,11 +87,11 @@ class BagCourierTest < SequelTestCase
     )
 
     # Set up data
-    @object_id = "000001"
+    @digital_object_id = "000001"
     @repository_name = "fake-repository"
     @bag_id = BagCourier::BagId.new(
       repository: @repository_name,
-      object_id: @object_id,
+      digital_object_id: @digital_object_id,
       context: "context"
     )
     @bag_info = BagTag::BagInfoBagTag.new(
@@ -110,14 +110,14 @@ class BagCourierTest < SequelTestCase
     @bag_repo = BagRepository::BagDatabaseRepository.new
     @status_event_repo = StatusEventRepository::StatusEventDatabaseRepository.new
     @package_repo.create(
-      identifier: @object_id,
+      identifier: @digital_object_id,
       repository_name: @repository_name,
       updated_at: Time.now.utc
     )
     @bag_repo.create(
       identifier: @bag_id.to_s,
       group_part: 1,
-      repository_package_identifier: @object_id
+      repository_package_identifier: @digital_object_id
     )
   end
 
